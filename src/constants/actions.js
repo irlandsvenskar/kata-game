ac40e3d285fe6b84b4ec6df41540b54575c3c394
George Boyle <george@spotify.com>
2017-12-17 23:18:21 +0100

    Add functions for changing state

--- src/constants/actions.js
+++ src/constants/actions.js
@@ -1,2 +1,10 @@
 export const COMPLETE_TASK = 'COMPLETE_TASK';
 export const START_EXERCISE = 'START_EXERCISE';
+
+export const startExercise = time => {
+    return {type: START_EXERCISE, time};
+};
+
+export const completeTask = (index, time) => {
+    return {type: COMPLETE_TASK, index, time};
+};
--- src/reducers/Exercise.spec.js
+++ src/reducers/Exercise.spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { COMPLETE_TASK, START_EXERCISE } from '../constants/actions.js';
+import { completeTask, startExercise } from '../constants/actions.js';
 import { reduce } from './Exercise.js';
 import Exercise from '../model/Exercise.js';
 
@@ -8,8 +8,7 @@ describe('Reduce exercise', () => {
     it('should capture start time when starting an exercise', () => {
         const stateBefore = {exercise: new Exercise(null, [])};
         const stateAfter = {exercise: new Exercise(3, [])};
-        const action = {type: START_EXERCISE, time: 3};
-        const actual = reduce(stateBefore, action);
+        const actual = reduce(stateBefore, startExercise(3));
         expect(actual).to.deep.equal(stateAfter);
     });
 
@@ -20,8 +19,7 @@ describe('Reduce exercise', () => {
         const stateAfter = {exercise: new Exercise(0, [
             {name: '::irrelevant task name::', endTime: 20}
         ])};
-        const action = {type: COMPLETE_TASK, index: 0, time: 20};
-        const actual = reduce(stateBefore, action);
+        const actual = reduce(stateBefore, completeTask(0, 20));
         expect(actual).to.deep.equal(stateAfter);
     });
 
@@ -34,8 +32,7 @@ describe('Reduce exercise', () => {
             {name: '::irrelevant task name::'},
             {name: '::irrelevant task name::', endTime: 10}
         ])};
-        const action = {type: COMPLETE_TASK, index: 1, time: 10};
-        const actual = reduce(stateBefore, action);
+        const actual = reduce(stateBefore, completeTask(1, 10));
         expect(actual).to.deep.equal(stateAfter);
     });
 
