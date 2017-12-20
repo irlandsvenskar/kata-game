c67a0ed6d4ca37dd9bb92823b20af659bd9217ed
George Boyle <george@spotify.com>
2017-12-20 18:15:41 +0100

    Moved hard-coded data towards the tests

--- src/components/ExercisePage.jsx
+++ src/components/ExercisePage.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 export default class ExercisePage extends React.Component {
     render() {
+        const tasks = this.props.exercise.tasks;
+        var taskTitle = null;
+        var errorMessage = null;
+        if (tasks && tasks.length > 0) {
+            taskTitle = tasks[0].name;
+        } else {
+            errorMessage = 'This exercise has no tasks!';
+        }
         return (
             <div>
-                <h1>Task 1</h1>
-                <p>This exercise has no tasks!</p>
+                <h1>{taskTitle}</h1>
+                <p>{errorMessage}</p>
             </div>
         )
     }
--- src/components/ExercisePage.spec.js
+++ src/components/ExercisePage.spec.js
@@ -1,5 +1,6 @@
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme from 'enzyme';
+import Exercise from '../model/Exercise';
 import ExercisePage from './ExercisePage';
 import React from 'react';
 import { expect } from 'chai';
@@ -10,13 +11,15 @@ Enzyme.configure({adapter: new Adapter()});
 describe('<ExercisePage/>', () => {
 
     it('should display an error if there are no tasks', () => {
-        const wrapper = shallow(<ExercisePage/>);
+        const exercise = new Exercise(0, []);
+        const wrapper = shallow(<ExercisePage exercise={exercise}/>);
         const error = wrapper.find('p').text();
         expect(error).to.equal('This exercise has no tasks!');
     });
 
     it('should show the first task', () => {
-        const wrapper = shallow(<ExercisePage/>);
+        const exercise = new Exercise(0, [{'name': 'Task 1'}]);
+        const wrapper = shallow(<ExercisePage exercise={exercise}/>);
         const heading = wrapper.find('h1').text();
         expect(heading).to.equal('Task 1');
     });
