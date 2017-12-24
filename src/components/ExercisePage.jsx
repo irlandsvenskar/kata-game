8d885c83891613cc1011fc4f56cf2af31658fb50
George Boyle <george@thebuds.net>
2017-12-24 12:26:48 +0100

    Use === for equality test

--- src/components/ExercisePage.jsx
+++ src/components/ExercisePage.jsx
@@ -5,7 +5,7 @@ export default class ExercisePage extends React.Component {
         const tasks = this.props.exercise.tasks;
         var taskTitle = null;
         var errorMessage = null;
-        if (!tasks || tasks.length == 0) {
+        if (!tasks || tasks.length === 0) {
             errorMessage = 'This exercise has no tasks!';
         } else {
             taskTitle = tasks[0].name;
