0ae007a3c991a295efc23d2275e247e0643d3d84
George Boyle <george@thebuds.net>
2017-12-24 19:48:27 +0100

    The start button can react to the click event

---
+++ src/components/StartButton.spec.js
@@ -0,0 +1,34 @@
+import Adapter from 'enzyme-adapter-react-16';
+import LandingPage from './LandingPage';
+import React from 'react';
+import { expect } from 'chai';
+import { configure, shallow } from 'enzyme';
+import { startExercise } from '../constants/actions';
+
+configure({adapter: new Adapter()});
+
+describe('start button', () => {
+
+    it('should emit the START_EXERCISE action', () => {
+        class MockActionDispatcher {
+            dispatch(action) {
+                this.action = action;
+            }
+            getAction() {
+                return this.action;
+            }
+        };
+        const actionDispatcher = new MockActionDispatcher();
+        const wrapper = shallow(
+            <LandingPage actionDispatcher={actionDispatcher}/>
+        );
+        const startButton = wrapper.find('button');
+        startButton.simulate('click');
+        const action = actionDispatcher.getAction();
+        const expectedAction = startExercise(123);
+
+        expect(action.time).to.equal(expectedAction.time);
+        expect(action.type).to.equal(expectedAction.type);
+    });
+
+});
