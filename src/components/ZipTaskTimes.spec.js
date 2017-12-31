2fa22efb0f0581be9960508e81c3cedeeca297b3
George Boyle <george@thebuds.net>
2017-12-31 09:31:06 +0100

    Can now zip a single task and task time

--- src/components/ZipTaskTimes.spec.js
+++ src/components/ZipTaskTimes.spec.js
@@ -1,8 +1,12 @@
 import Exercise from '../model/Exercise';
+import Task from '../model/Task';
 import { expect } from 'chai';
 
 const zipTaskTimes = (tasks, taskTimes) => {
-    return [];
+    if (tasks.length === 0)
+        return [];
+    else
+        return [{'taskName': tasks[0].title, 'taskFinishTime': taskTimes[0]}];
 }
 
 describe('Zip task names and times', () => {
@@ -12,9 +16,23 @@ describe('Zip task names and times', () => {
             '::irrelevant title::', '::irrelevant instructions::', []);
         const taskTimes = [];
 
-        const zipped = zipTaskTimes(exercise, taskTimes);
+        const zipped = zipTaskTimes(exercise.tasks, taskTimes);
 
         expect(zipped).to.deep.equal([]);
+    });
+
+    it('should zip singleton task and time', () => {
+        const irrelevantValue = 1234;
+        const exercise = new Exercise(
+            '::irrelevant title::', '::irrelevant instructions::',
+            [new Task('::task name::', '::irrelevant instructions::')]);
+        const taskTimes = [irrelevantValue];
+
+        const zipped = zipTaskTimes(exercise.tasks, taskTimes);
+
+        expect(zipped).to.deep.equal([
+            {'taskName': '::task name::', 'taskFinishTime': irrelevantValue}
+        ]);
     });
 
 });
