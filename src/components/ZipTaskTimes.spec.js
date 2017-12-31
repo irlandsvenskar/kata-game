17ac561df411be167689a7f3c91bbe757c1c2e58
George Boyle <george@thebuds.net>
2017-12-31 12:09:37 +0100

    Can zip tasks where times are longer than tasks

--- src/components/ZipTaskTimes.spec.js
+++ src/components/ZipTaskTimes.spec.js
@@ -1,10 +1,10 @@
-import Exercise from '../model/Exercise';
 import Task from '../model/Task';
 import { expect } from 'chai';
 
 const zipTaskTimes = (tasks, taskTimes) => {
     var zipped = [];
-    for (var i = 0; i < tasks.length; i++) {
+    const length = Math.min(tasks.length, taskTimes.length);
+    for (var i = 0; i < length; i++) {
         zipped.push({
             'taskName': tasks[i].title,
             'taskFinishTime': taskTimes[i]
@@ -62,6 +62,21 @@ describe('Zip task names and times', () => {
 
         expect(zipped).to.deep.equal([
             {'taskName': '::task name::', 'taskFinishTime': irrelevantTime1},
+        ]);
+    });
+
+    it('should zip where tasks is shorter than times', () => {
+        const irrelevantTime = 12;
+        const tasks = [
+            new Task('::task 1::', '::irrelevant instructions::'),
+            new Task('::task 2::', '::irrelevant instructions::')
+        ];
+        const taskTimes = [irrelevantTime];
+
+        const zipped = zipTaskTimes(tasks, taskTimes);
+
+        expect(zipped).to.deep.equal([
+            {'taskName': '::task 1::', 'taskFinishTime': irrelevantTime},
         ]);
     });
 
