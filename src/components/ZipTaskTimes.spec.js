e213f70f713364f808eab91a04b0f1cda9df54a7
George Boyle <george@thebuds.net>
2017-12-31 09:35:59 +0100

    Remove unneeded code in tests

--- src/components/ZipTaskTimes.spec.js
+++ src/components/ZipTaskTimes.spec.js
@@ -12,23 +12,17 @@ const zipTaskTimes = (tasks, taskTimes) => {
 describe('Zip task names and times', () => {
 
     it('should zip empty', () => {
-        const exercise = new Exercise(
-            '::irrelevant title::', '::irrelevant instructions::', []);
-        const taskTimes = [];
-
-        const zipped = zipTaskTimes(exercise.tasks, taskTimes);
-
-        expect(zipped).to.deep.equal([]);
+        expect(zipTaskTimes([], [])).to.deep.equal([]);
     });
 
     it('should zip singleton task and time', () => {
         const irrelevantValue = 1234;
-        const exercise = new Exercise(
-            '::irrelevant title::', '::irrelevant instructions::',
-            [new Task('::task name::', '::irrelevant instructions::')]);
+        const tasks = [
+            new Task('::task name::', '::irrelevant instructions::')
+        ];
         const taskTimes = [irrelevantValue];
 
-        const zipped = zipTaskTimes(exercise.tasks, taskTimes);
+        const zipped = zipTaskTimes(tasks, taskTimes);
 
         expect(zipped).to.deep.equal([
             {'taskName': '::task name::', 'taskFinishTime': irrelevantValue}
